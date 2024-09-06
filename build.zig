36220a8868cb2dce661fae58b240c01d22076062
Timothée Delabrouille <34384633+tdelabro@users.noreply.github.com>
2024-09-06 10:53:21 +0200

    tests: build them in Debug mode (#15)

--- build.zig
+++ build.zig
@@ -101,7 +101,6 @@ pub fn build(b: *std.Build) !void {
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/lib.zig"),
         .target = target,
-        .optimize = optimize,
     });
 
     // Add dependency modules to the library.
@@ -115,7 +114,6 @@ pub fn build(b: *std.Build) !void {
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
-        .optimize = optimize,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
