a8e3c3b060b666e15e68ecbdbfa1601b395e5abc
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2024-09-16 10:49:28 +0200

    feat(p2p): add wire package for messages io

--- src/network/protocol/messages/lib.zig
+++ src/network/protocol/messages/lib.zig
@@ -1,1 +1,14 @@
+const std = @import("std");
 pub const VersionMessage = @import("version.zig").VersionMessage;
+
+pub const MessageTypes = enum { Version };
+
+pub const Message = union(MessageTypes) {
+    Version: VersionMessage,
+
+    pub fn deinit(self: Message, allocator: std.mem.Allocator) void {
+        switch (self) {
+            .Version => |m| m.deinit(allocator),
+        }
+    }
+};
--- src/network/protocol/messages/version.zig
+++ src/network/protocol/messages/version.zig
@@ -2,12 +2,10 @@ const std = @import("std");
 const native_endian = @import("builtin").target.cpu.arch.endian();
 const protocol = @import("../lib.zig");
 
-const NetworkAddress = protocol.NetworkAddress;
 const ServiceFlags = protocol.ServiceFlags;
-const IpV6Address = protocol.IpV6Address;
-const Message = protocol.message.Message;
 
 const Endian = std.builtin.Endian;
+const Sha256 = std.crypto.hash.sha2.Sha256;
 
 const CompactSizeUint = @import("bitcoin-primitives").types.CompatSizeUint;
 
@@ -15,187 +13,213 @@ const CompactSizeUint = @import("bitcoin-primitives").types.CompatSizeUint;
 ///
 /// https://developer.bitcoin.org/reference/p2p_networking.html#version
 pub const VersionMessage = struct {
-    version: i32,
-    services: u64,
+    recv_ip: [16]u8,
+    trans_ip: [16]u8,
     timestamp: i64,
-    addr_recv: NetworkAddress,
-    addr_trans: NetworkAddress,
+    services: u64,
     nonce: u64,
-    user_agent: ?[]const u8,
+    recv_services: u64,
+    trans_services: u64,
+    version: i32,
     start_height: i32,
+    recv_port: u16,
+    trans_port: u16,
+    user_agent: ?[]const u8,
     relay: ?bool,
 
-    /// Will free the user_agent if present in the message
+    pub fn name() [12]u8 {
+        var ret = std.mem.zeroes([12]u8);
+        @memcpy(ret[0..7], protocol.CommandNames.VERSION);
+        return ret;
+    }
+
+    /// Returns the message checksum
+    ///
+    /// Computed as `Sha256(Sha256(self.serialize()))[0..4]`
+    pub fn checksum(self: VersionMessage) [4]u8 {
+        var digest: [32]u8 = undefined;
+        var hasher = Sha256.init(.{});
+        const writer = hasher.writer();
+        self.serializeToWriter(writer) catch unreachable; // Sha256.write is infaible
+        hasher.final(&digest);
+
+        Sha256.hash(&digest, &digest, .{});
+
+        var ret: [4]u8 = undefined;
+        @memcpy(ret[0..], digest[0..4]);
+
+        return ret;
+    }
+
+    /// Free the `user_agent` if there is one
     pub fn deinit(self: VersionMessage, allocator: std.mem.Allocator) void {
         if (self.user_agent) |ua| {
             allocator.free(ua);
         }
     }
 
-    /// Serialize a message to bytes
+    /// Serialize the message as bytes and write them to the Writer.
     ///
-    /// The caller is responsible for freeing the returned value.
-    pub fn serializeTo(self: VersionMessage, buffer: []u8) void {
+    /// `w` should be a valid `Writer`.
+    pub fn serializeToWriter(self: *const VersionMessage, w: anytype) !void {
+        comptime {
+            if (!std.meta.hasFn(@TypeOf(w), "writeInt")) @compileError("Expects r to have fn 'writeInt'.");
+            if (!std.meta.hasFn(@TypeOf(w), "writeAll")) @compileError("Expects r to have fn 'writeAll'.");
+        }
+
         const user_agent_len: usize = if (self.user_agent) |ua|
             ua.len
         else
             0;
         const compact_user_agent_len = CompactSizeUint.new(user_agent_len);
-        const compact_user_agent_len_len = compact_user_agent_len.hint_encoded_len();
 
-        copyWithEndian(buffer[0..4], std.mem.asBytes(&self.version), .little);
-        copyWithEndian(buffer[4..12], std.mem.asBytes(&self.services), .little);
-        copyWithEndian(buffer[12..20], std.mem.asBytes(&self.timestamp), .little);
-        copyWithEndian(buffer[20..28], std.mem.asBytes(&self.addr_recv.services), .little);
-        @memcpy(buffer[28..44], std.mem.asBytes(&self.addr_recv.address.ip)); // ip is already repr as big endian
-        copyWithEndian(buffer[44..46], std.mem.asBytes(&self.addr_recv.address.port), .big);
-        copyWithEndian(buffer[46..54], std.mem.asBytes(&self.addr_trans.services), .little);
-        @memcpy(buffer[54..70], std.mem.asBytes(&self.addr_trans.address.ip)); // ip is already repr as big endian
-        copyWithEndian(buffer[70..72], std.mem.asBytes(&self.addr_trans.address.port), .big);
-        copyWithEndian(buffer[72..80], std.mem.asBytes(&self.nonce), .little);
-        compact_user_agent_len.encode_to(buffer[80..]);
+        try w.writeInt(i32, self.version, .little);
+        try w.writeInt(u64, self.services, .little);
+        try w.writeInt(i64, self.timestamp, .little);
+        try w.writeInt(u64, self.recv_services, .little);
+        try w.writeAll(std.mem.asBytes(&self.recv_ip));
+        try w.writeInt(u16, self.recv_port, .big);
+        try w.writeInt(u64, self.trans_services, .little);
+        try w.writeAll(std.mem.asBytes(&self.trans_ip));
+        try w.writeInt(u16, self.trans_port, .big);
+        try w.writeInt(u64, self.nonce, .little);
+        try compact_user_agent_len.encodeToWriter(w);
         if (user_agent_len != 0) {
-            @memcpy(buffer[80 + compact_user_agent_len_len .. 80 + compact_user_agent_len_len + user_agent_len], self.user_agent.?);
+            try w.writeAll(self.user_agent.?);
         }
-        copyWithEndian(buffer[80 + compact_user_agent_len_len + user_agent_len .. 80 + compact_user_agent_len_len + user_agent_len + 4], std.mem.asBytes(&self.start_height), .little);
-        if (self.relay) |relay| {
-            copyWithEndian(buffer[80 + compact_user_agent_len_len + user_agent_len + 4 .. 80 + compact_user_agent_len_len + user_agent_len + 4 + 1], std.mem.asBytes(&relay), .little);
+        try w.writeInt(i32, self.start_height, .little);
+        if (self.relay) |r| {
+            try w.writeAll(std.mem.asBytes(&r));
         }
     }
 
-    /// Serialize a message to bytes
+    /// Serialize a message as bytes and write them to the buffer.
     ///
-    /// The caller is responsible for freeing the returned value.
-    pub fn serialize(self: VersionMessage, allocator: std.mem.Allocator) ![]u8 {
+    /// buffer.len must be >= than self.hintSerializedLen()
+    pub fn serializeToSlice(self: *const VersionMessage, buffer: []u8) !void {
+        var fbs = std.io.fixedBufferStream(buffer);
+        const writer = fbs.writer();
+        try self.serializeToWriter(writer);
+    }
+
+    /// Serialize a message as bytes and return them.
+    pub fn serialize(self: *const VersionMessage, allocator: std.mem.Allocator) ![]u8 {
         const serialized_len = self.hintSerializedLen();
 
-        const res = try allocator.alloc(u8, serialized_len);
+        const ret = try allocator.alloc(u8, serialized_len);
+        errdefer allocator.free(ret);
 
-        self.serializeTo(res);
+        try self.serializeToSlice(ret);
 
-        return res;
+        return ret;
     }
 
-    pub const DeserializeError = error{
-        InputTooShort,
-    };
+    /// Deserialize a Reader bytes as a `VersionMessage`
+    pub fn deserializeReader(allocator: std.mem.Allocator, r: anytype) !VersionMessage {
+        comptime {
+            if (!std.meta.hasFn(@TypeOf(r), "readInt")) @compileError("Expects r to have fn 'readInt'.");
+            if (!std.meta.hasFn(@TypeOf(r), "readNoEof")) @compileError("Expects r to have fn 'readNoEof'.");
+            if (!std.meta.hasFn(@TypeOf(r), "readAll")) @compileError("Expects r to have fn 'readAll'.");
+            if (!std.meta.hasFn(@TypeOf(r), "readByte")) @compileError("Expects r to have fn 'readByte'.");
+        }
 
-    /// Deserialize bytes into a `VersionMessage`
-    ///
-    /// The caller is responsible for freeing the allocated memory in field `user_agent` by calling `VersionMessage.deinit();`
-    pub fn deserialize(allocator: std.mem.Allocator, bytes: []const u8) !VersionMessage {
         var vm: VersionMessage = undefined;
 
-        // No Version can be shorter than this
-        if (bytes.len < 85) {
-            return error.InputTooShort;
-        }
-        const compact_user_agent_len = try CompactSizeUint.decode(bytes[80..]);
-        const user_agent_len = compact_user_agent_len.value();
-        const compact_user_agent_len_len = compact_user_agent_len.hint_encoded_len();
+        vm.version = try r.readInt(i32, .little);
+        vm.services = try r.readInt(u64, .little);
+        vm.timestamp = try r.readInt(i64, .little);
+        vm.recv_services = try r.readInt(u64, .little);
+        try r.readNoEof(&vm.recv_ip);
+        vm.recv_port = try r.readInt(u16, .big);
+        vm.trans_services = try r.readInt(u64, .little);
+        try r.readNoEof(&vm.trans_ip);
+        vm.trans_port = try r.readInt(u16, .big);
+        vm.nonce = try r.readInt(u64, .little);
 
-        copyWithEndian(std.mem.asBytes(&vm.version), bytes[0..4], .little);
-        copyWithEndian(std.mem.asBytes(&vm.services), bytes[4..12], .little);
-        copyWithEndian(std.mem.asBytes(&vm.timestamp), bytes[12..20], .little);
-        copyWithEndian(std.mem.asBytes(&vm.addr_recv.services), bytes[20..28], .little);
-        @memcpy(std.mem.asBytes(&vm.addr_recv.address.ip), bytes[28..44]); // ip already in big endian
-        copyWithEndian(std.mem.asBytes(&vm.addr_recv.address.port), bytes[44..46], .big);
-        copyWithEndian(std.mem.asBytes(&vm.addr_trans.services), bytes[46..54], .little);
-        @memcpy(std.mem.asBytes(&vm.addr_trans.address.ip), bytes[54..70]); // ip already in big endian
-        copyWithEndian(std.mem.asBytes(&vm.addr_trans.address.port), bytes[70..72], .big);
-        copyWithEndian(std.mem.asBytes(&vm.nonce), bytes[72..80], .little);
+        const user_agent_len = (try CompactSizeUint.decodeReader(r)).value();
+
         if (user_agent_len != 0) {
             const user_agent = try allocator.alloc(u8, user_agent_len);
-            @memcpy(user_agent, bytes[80 + compact_user_agent_len_len .. 80 + compact_user_agent_len_len + user_agent_len]);
+            errdefer allocator.free(user_agent);
+            try r.readNoEof(user_agent);
             vm.user_agent = user_agent;
         } else {
             vm.user_agent = null;
         }
-        copyWithEndian(std.mem.asBytes(&vm.start_height), bytes[80 + compact_user_agent_len_len + user_agent_len .. 80 + compact_user_agent_len_len + user_agent_len + 4], .little);
-        if (bytes.len == 80 + compact_user_agent_len_len + user_agent_len + 4 + 1) {
-            copyWithEndian(std.mem.asBytes(&vm.relay.?), bytes[80 + compact_user_agent_len_len + user_agent_len + 4 .. 80 + compact_user_agent_len_len + user_agent_len + 4 + 1], .little);
-        } else {
-            vm.relay = null;
-        }
+        vm.start_height = try r.readInt(i32, .little);
+        vm.relay = if (r.readByte() catch null) |v| v != 0 else null;
 
         return vm;
+    }
+
+    /// Deserialize bytes into a `VersionMessage`
+    pub fn deserializeSlice(allocator: std.mem.Allocator, bytes: []const u8) !VersionMessage {
+        var fbs = std.io.fixedBufferStream(bytes);
+        const reader = fbs.reader();
+        return try VersionMessage.deserializeReader(allocator, reader);
     }
 
     pub fn hintSerializedLen(self: VersionMessage) usize {
         // 4 + 8 + 8 + (2 * (8 + 16 + 2) + 8 + 4)
         const fixed_length = 84;
-        const user_agent_len: usize = if (self.user_agent) |ua|
-            ua.len
-        else
-            0;
+        const user_agent_len: usize = if (self.user_agent) |ua| ua.len else 0;
         const compact_user_agent_len = CompactSizeUint.new(user_agent_len);
         const compact_user_agent_len_len = compact_user_agent_len.hint_encoded_len();
         const relay_len: usize = if (self.relay != null) 1 else 0;
         const variable_length = compact_user_agent_len_len + user_agent_len + relay_len;
         return fixed_length + variable_length;
     }
+
+    pub fn eql(self: *const VersionMessage, other: *const VersionMessage) bool {
+        // Normal fields
+        if (self.version != other.version //
+        or self.services != other.services //
+        or self.timestamp != other.timestamp //
+        or self.recv_services != other.recv_services //
+        or !std.mem.eql(u8, &self.recv_ip, &other.recv_ip) //
+        or self.recv_port != other.recv_port //
+        or self.trans_services != other.trans_services //
+        or !std.mem.eql(u8, &self.trans_ip, &other.trans_ip) //
+        or self.trans_port != other.trans_port //
+        or self.nonce != other.nonce) {
+            return false;
+        }
+
+        // user_agent
+        if (self.user_agent) |lua| {
+            if (other.user_agent) |rua| {
+                if (!std.mem.eql(u8, lua, rua)) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            if (other.user_agent) |_| {
+                return false;
+            }
+        }
+
+        // relay
+        if (self.relay) |ln| {
+            if (other.relay) |rn| {
+                if (ln != rn) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            if (other.relay) |_| {
+                return false;
+            }
+        }
+
+        return true;
+    }
 };
 
-// Copy to dest and apply the specified endianness
-//
-// dest and src should not overlap
-// dest.len should be == to src.len
-fn copyWithEndian(dest: []u8, src: []const u8, endian: Endian) void {
-    @memcpy(dest, src);
-    if (native_endian != endian) {
-        std.mem.reverse(u8, dest[0..src.len]);
-    }
-}
-
 // TESTS
-
-fn compareVersionMessage(lhs: VersionMessage, rhs: VersionMessage) bool {
-    // Normal fields
-    if (lhs.version != rhs.version //
-    or lhs.services != rhs.services //
-    or lhs.timestamp != rhs.timestamp //
-    or lhs.addr_recv.services != rhs.addr_recv.services //
-    or !std.mem.eql(u16, &lhs.addr_recv.address.ip, &rhs.addr_recv.address.ip) //
-    or lhs.addr_recv.address.port != rhs.addr_recv.address.port //
-    or lhs.addr_trans.services != rhs.addr_trans.services //
-    or !std.mem.eql(u16, &lhs.addr_trans.address.ip, &rhs.addr_trans.address.ip) //
-    or lhs.addr_trans.address.port != rhs.addr_trans.address.port //
-    or lhs.nonce != rhs.nonce) {
-        return false;
-    }
-
-    // user_agent
-    if (lhs.user_agent) |lua| {
-        if (rhs.user_agent) |rua| {
-            if (!std.mem.eql(u8, lua, rua)) {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    } else {
-        if (rhs.user_agent) |_| {
-            return false;
-        }
-    }
-
-    // relay
-    if (lhs.relay) |ln| {
-        if (rhs.relay) |rn| {
-            if (ln != rn) {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    } else {
-        if (rhs.relay) |_| {
-            return false;
-        }
-    }
-
-    return true;
-}
 
 test "ok_full_flow_VersionMessage" {
     const allocator = std.testing.allocator;
@@ -206,20 +230,12 @@ test "ok_full_flow_VersionMessage" {
             .version = 42,
             .services = ServiceFlags.NODE_NETWORK,
             .timestamp = 43,
-            .addr_recv = NetworkAddress{
-                .services = ServiceFlags.NODE_WITNESS,
-                .address = IpV6Address{
-                    .ip = [_]u16{13} ** 8,
-                    .port = 17,
-                },
-            },
-            .addr_trans = NetworkAddress{
-                .services = ServiceFlags.NODE_BLOOM,
-                .address = IpV6Address{
-                    .ip = [_]u16{13} ** 8,
-                    .port = 19,
-                },
-            },
+            .recv_services = ServiceFlags.NODE_WITNESS,
+            .trans_services = ServiceFlags.NODE_BLOOM,
+            .recv_ip = [_]u8{13} ** 16,
+            .trans_ip = [_]u8{12} ** 16,
+            .recv_port = 33,
+            .trans_port = 22,
             .nonce = 31,
             .user_agent = null,
             .start_height = 1000,
@@ -228,10 +244,10 @@ test "ok_full_flow_VersionMessage" {
 
         const payload = try vm.serialize(allocator);
         defer allocator.free(payload);
-        const deserialized_vm = try VersionMessage.deserialize(allocator, payload);
+        const deserialized_vm = try VersionMessage.deserializeSlice(allocator, payload);
         defer deserialized_vm.deinit(allocator);
 
-        try std.testing.expect(compareVersionMessage(vm, deserialized_vm));
+        try std.testing.expect(vm.eql(&deserialized_vm));
     }
 
     // With relay
@@ -240,20 +256,12 @@ test "ok_full_flow_VersionMessage" {
             .version = 42,
             .services = ServiceFlags.NODE_NETWORK,
             .timestamp = 43,
-            .addr_recv = NetworkAddress{
-                .services = ServiceFlags.NODE_WITNESS,
-                .address = IpV6Address{
-                    .ip = [_]u16{13} ** 8,
-                    .port = 17,
-                },
-            },
-            .addr_trans = NetworkAddress{
-                .services = ServiceFlags.NODE_BLOOM,
-                .address = IpV6Address{
-                    .ip = [_]u16{13} ** 8,
-                    .port = 19,
-                },
-            },
+            .recv_services = ServiceFlags.NODE_WITNESS,
+            .trans_services = ServiceFlags.NODE_BLOOM,
+            .recv_ip = [_]u8{13} ** 16,
+            .trans_ip = [_]u8{12} ** 16,
+            .recv_port = 33,
+            .trans_port = 22,
             .nonce = 31,
             .user_agent = null,
             .start_height = 1000,
@@ -262,10 +270,10 @@ test "ok_full_flow_VersionMessage" {
 
         const payload = try vm.serialize(allocator);
         defer allocator.free(payload);
-        const deserialized_vm = try VersionMessage.deserialize(allocator, payload);
+        const deserialized_vm = try VersionMessage.deserializeSlice(allocator, payload);
         defer deserialized_vm.deinit(allocator);
 
-        try std.testing.expect(compareVersionMessage(vm, deserialized_vm));
+        try std.testing.expect(vm.eql(&deserialized_vm));
     }
 
     // With relay and user agent
@@ -275,20 +283,12 @@ test "ok_full_flow_VersionMessage" {
             .version = 42,
             .services = ServiceFlags.NODE_NETWORK,
             .timestamp = 43,
-            .addr_recv = NetworkAddress{
-                .services = ServiceFlags.NODE_WITNESS,
-                .address = IpV6Address{
-                    .ip = [_]u16{13} ** 8,
-                    .port = 17,
-                },
-            },
-            .addr_trans = NetworkAddress{
-                .services = ServiceFlags.NODE_BLOOM,
-                .address = IpV6Address{
-                    .ip = [_]u16{13} ** 8,
-                    .port = 19,
-                },
-            },
+            .recv_services = ServiceFlags.NODE_WITNESS,
+            .trans_services = ServiceFlags.NODE_BLOOM,
+            .recv_ip = [_]u8{13} ** 16,
+            .trans_ip = [_]u8{12} ** 16,
+            .recv_port = 33,
+            .trans_port = 22,
             .nonce = 31,
             .user_agent = &user_agent,
             .start_height = 1000,
@@ -297,9 +297,9 @@ test "ok_full_flow_VersionMessage" {
 
         const payload = try vm.serialize(allocator);
         defer allocator.free(payload);
-        const deserialized_vm = try VersionMessage.deserialize(allocator, payload);
+        const deserialized_vm = try VersionMessage.deserializeSlice(allocator, payload);
         defer deserialized_vm.deinit(allocator);
 
-        try std.testing.expect(compareVersionMessage(vm, deserialized_vm));
+        try std.testing.expect(vm.eql(&deserialized_vm));
     }
 }
