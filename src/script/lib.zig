69db09d0c454d20247e5907e442b801e4fa99e97
KhairallahA <khairallahfreelancer@gmail.com>
2024-09-18 15:38:34 +0300

    resolved conflict

--- src/script/lib.zig
+++ src/script/lib.zig
@@ -1,6 +1,9 @@
 pub const engine = @import("engine.zig");
 pub const stack = @import("stack.zig");
 pub const arithmetic = @import("opcodes/arithmetic.zig");
+const StackError = @import("stack.zig").StackError;
+
+pub const ScriptNum = i64;
 
 /// Maximum number of bytes pushable to the stack
 const MAX_SCRIPT_ELEMENT_SIZE = 520;
@@ -61,3 +64,17 @@ pub const Script = struct {
         return self.len() == 0;
     }
 };
+
+/// Errors that can occur during script execution
+pub const EngineError = error{
+    /// Script ended unexpectedly
+    ScriptTooShort,
+    /// OP_VERIFY failed
+    VerifyFailed,
+    /// OP_RETURN encountered
+    EarlyReturn,
+    /// Encountered an unknown opcode
+    UnknownOpcode,
+    /// Encountered a disabled opcode
+    DisabledOpcode,
+} || StackError;
