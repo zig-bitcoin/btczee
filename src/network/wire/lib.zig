15f24e21114e744837eb51cccf1c94b95c595a64
Nathan GD <53536851+gdnathan@users.noreply.github.com>
2024-09-20 21:48:05 +0200

    corrected wrong link (#112)
    Co-authored-by: Timothée Delabrouille <34384633+tdelabro@users.noreply.github.com>

--- src/network/wire/lib.zig
+++ src/network/wire/lib.zig
@@ -49,7 +49,7 @@ pub fn sendMessage(allocator: std.mem.Allocator, w: anytype, protocol_version: i
     defer allocator.free(payload);
     const checksum = computePayloadChecksum(payload);
 
-    // I believe it's safe. No payload will be longer than u32.MAX
+    // No payload will be longer than u32.MAX
     const payload_len: u32 = @intCast(payload.len);
 
     try w.writeAll(&network_id);
@@ -79,9 +79,10 @@ pub fn receiveMessage(allocator: std.mem.Allocator, r: anytype) !protocol.messag
     const message: protocol.messages.Message = if (std.mem.eql(u8, &command, protocol.messages.VersionMessage.name()))
         protocol.messages.Message{ .Version = try protocol.messages.VersionMessage.deserializeReader(allocator, r) }
     else if (std.mem.eql(u8, &command, protocol.messages.VerackMessage.name()))
-        protocol.messages.Message{ .Verack = try protocol.messages.VerackMessage.deserializeReader(allocator, r)}
+
+        protocol.messages.Message{ .Verack = try protocol.messages.VerackMessage.deserializeReader(allocator, r) }
     else if (std.mem.eql(u8, &command, protocol.messages.MempoolMessage.name()))
-        protocol.messages.Message{ .Mempool = try protocol.messages.MempoolMessage.deserializeReader(allocator, r)}
+        protocol.messages.Message{ .Mempool = try protocol.messages.MempoolMessage.deserializeReader(allocator, r) }
     else if (std.mem.eql(u8, &command, protocol.messages.GetaddrMessage.name()))
         protocol.messages.Message{ .Getaddr = try protocol.messages.GetaddrMessage.deserializeReader(allocator, r) }
     else
