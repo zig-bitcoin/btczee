18cabaf2bd76bbecd8c3318a9426515e25bb1223
oxlime <sean.gne.19@gmail.com>
2024-09-19 19:53:48 -0500

    mempool message

--- src/network/protocol/messages/lib.zig
+++ src/network/protocol/messages/lib.zig
@@ -1,23 +1,27 @@
 const std = @import("std");
 pub const VersionMessage = @import("version.zig").VersionMessage;
 pub const VerackMessage = @import("verack.zig").VerackMessage;
+pub const MempoolMessage = @import("mempool.zig").MempoolMessage;
 
-pub const MessageTypes = enum { Version, Verack };
+pub const MessageTypes = enum { Version, Verack, Mempool };
 
 pub const Message = union(MessageTypes) {
     Version: VersionMessage,
     Verack: VerackMessage,
+    Mempool: MempoolMessage,
 
     pub fn deinit(self: Message, allocator: std.mem.Allocator) void {
         switch (self) {
             .Version => |m| m.deinit(allocator),
             .Verack => {},
+            .Mempool => {},
         }
     }
     pub fn checksum(self: Message) [4]u8 {
         return switch (self) {
             .Version => |m| m.checksum(),
             .Verack => |m| m.checksum(),
+            .Mempool => |m| m.checksum(),
         };
     }
 
@@ -25,6 +29,7 @@ pub const Message = union(MessageTypes) {
         return switch (self) {
             .Version => |m| m.hintSerializedLen(),
             .Verack => |m| m.hintSerializedLen(),
+            .Mempool => |m| m.hintSerializedLen(),
         };
     }
 };
