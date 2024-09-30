b974d72285e2d02e073cdb4ac9110cef5bfc8403
supreme2580 <victoromorogbe69@gmail.com>
2024-09-30 20:39:15 +0100

    notfound

--- src/network/protocol/messages/lib.zig
+++ src/network/protocol/messages/lib.zig
@@ -18,14 +18,27 @@ pub const InventoryVector = struct {
     hash: [32]u8,
 
     pub fn serializeToWriter(self: InventoryVector, writer: anytype) !void {
+        comptime {
+            if (!std.meta.hasFn(@TypeOf(writer), "writeInt")) @compileError("Expects writer to have fn 'writeInt'.");
+            if (!std.meta.hasFn(@TypeOf(writer), "writeAll")) @compileError("Expects writer to have fn 'writeAll'.");
+        }
         try writer.writeInt(u32, self.type, .little);
         try writer.writeAll(&self.hash);
     }
 
     pub fn deserializeReader(r: anytype) !InventoryVector {
+        comptime {
+            if (!std.meta.hasFn(@TypeOf(r), "readInt")) @compileError("Expects r to have fn 'readInt'.");
+            if (!std.meta.hasFn(@TypeOf(r), "readBytesNoEof")) @compileError("Expects r to have fn 'readBytesNoEof'.");
+        }
+
+        const type_value = try r.readInt(u32, .little);
+        var hash: [32]u8 = undefined;
+        try r.readNoEof(&hash);
+
         return InventoryVector{
-            .type = try r.readInt(u32, .little),
-            .hash = try r.readBytesNoEof(32),
+            .type = type_value,
+            .hash = hash,
         };
     }
 };
