9303fd895adbd3ac5ab13a20258902031c7dcc68
supreme2580 <victoromorogbe69@gmail.com>
2024-10-02 07:10:32 +0100

    added to wire/lib.zig

--- src/network/wire/lib.zig
+++ src/network/wire/lib.zig
@@ -141,6 +141,8 @@ pub fn receiveMessage(
         protocol.messages.Message{ .sendheaders = try protocol.messages.SendHeadersMessage.deserializeReader(allocator, r) }
     else if (std.mem.eql(u8, &command, protocol.messages.FilterLoadMessage.name()))
         protocol.messages.Message{ .filterload = try protocol.messages.FilterLoadMessage.deserializeReader(allocator, r) }
+    else if (std.mem.eql(u8, &command, protocol.messages.CmpctBlockMessage.name()))
+        protocol.messages.Message{ .cmpctblock = try protocol.messages.CmpctBlockMessage.deserializeReader(allocator, r) }
     else {
         try r.skipBytes(payload_len, .{}); // Purge the wire
         return error.UnknownMessage;
