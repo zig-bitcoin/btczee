e95cf1ea6b15f53e0db982739eb1737d71a7692e
Patrice Tisserand <p.tisserand@gmail.com>
2024-10-03 01:48:15 +0200

    remove useless comptime check and use `BlockHeader::eql` instead of field by field compare.

--- src/network/protocol/messages/headers.zig
+++ src/network/protocol/messages/headers.zig
@@ -31,8 +31,6 @@ pub const HeadersMessage = struct {
     /// `w` should be a valid `Writer`.
     pub fn serializeToWriter(self: *const Self, w: anytype) !void {
         comptime {
-            if (!std.meta.hasFn(@TypeOf(w), "writeInt")) @compileError("Expects r to have fn 'writeInt'.");
-            if (!std.meta.hasFn(@TypeOf(w), "writeAll")) @compileError("Expects r to have fn 'writeAll'.");
             if (!std.meta.hasFn(@TypeOf(w), "writeByte")) @compileError("Expects r to have fn 'writeByte'.");
         }
         try CompactSizeUint.new(self.headers.len).encodeToWriter(w);
@@ -68,9 +66,6 @@ pub const HeadersMessage = struct {
 
     pub fn deserializeReader(allocator: std.mem.Allocator, r: anytype) !Self {
         comptime {
-            if (!std.meta.hasFn(@TypeOf(r), "readInt")) @compileError("Expects r to have fn 'readInt'.");
-            if (!std.meta.hasFn(@TypeOf(r), "readNoEof")) @compileError("Expects r to have fn 'readNoEof'.");
-            if (!std.meta.hasFn(@TypeOf(r), "readAll")) @compileError("Expects r to have fn 'readAll'.");
             if (!std.meta.hasFn(@TypeOf(r), "readByte")) @compileError("Expects r to have fn 'readByte'.");
         }
 
@@ -111,13 +106,14 @@ test "ok_fullflow_headers_message" {
             0x01, // header count
             // block header
             0x02, 0x00, 0x00, 0x00, // block version: 2
-            0xb6, 0xff, 0x0b, 0x1b,
-            0x16, 0x80, 0xa2, 0x86,
-            0x2a, 0x30, 0xca, 0x44,
-            0xd3, 0x46, 0xd9, 0xe8,
-            0x91, 0x0d, 0x33, 0x4b, 0xeb, 0x48, 0xca, 0x0c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // hash of previous block
-            0x9d, 0x10, 0xaa, 0x52, 0xee, 0x94, 0x93, 0x86, 0xca, 0x93, 0x85, 0x69, 0x5f, 0x04, 0xed, 0xe2,
-            0x70, 0xdd, 0xa2, 0x08, 0x10, 0xde, 0xcd, 0x12, 0xbc, 0x9b, 0x04, 0x8a, 0xaa, 0xb3, 0x14, 0x71, // merkle root
+            0xb6, 0xff, 0x0b, 0x1b, 0x16, 0x80, 0xa2, 0x86, // hash of previous block
+            0x2a, 0x30, 0xca, 0x44, 0xd3, 0x46, 0xd9, 0xe8, // hash of previous block
+            0x91, 0x0d, 0x33, 0x4b, 0xeb, 0x48, 0xca, 0x0c, // hash of previous block
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // hash of previous block
+            0x9d, 0x10, 0xaa, 0x52, 0xee, 0x94, 0x93, 0x86, // merkle root
+            0xca, 0x93, 0x85, 0x69, 0x5f, 0x04, 0xed, 0xe2, // merkle root
+            0x70, 0xdd, 0xa2, 0x08, 0x10, 0xde, 0xcd, 0x12, // merkle root
+            0xbc, 0x9b, 0x04, 0x8a, 0xaa, 0xb3, 0x14, 0x71, // merkle root
             0x24, 0xd9, 0x5a, 0x54, // unix time (1415239972)
             0x30, 0xc3, 0x1b, 0x18, // bits
             0xfe, 0x9f, 0x08, 0x64, // nonce
@@ -128,11 +124,28 @@ test "ok_fullflow_headers_message" {
         var deserialized_msg = try HeadersMessage.deserializeSlice(allocator, &payload);
         defer deserialized_msg.deinit(allocator);
 
+        const expected_block_header = BlockHeader{
+            .version = 2,
+            .prev_block = [_]u8{
+                0xb6, 0xff, 0x0b, 0x1b, 0x16, 0x80, 0xa2, 0x86,
+                0x2a, 0x30, 0xca, 0x44, 0xd3, 0x46, 0xd9, 0xe8,
+                0x91, 0x0d, 0x33, 0x4b, 0xeb, 0x48, 0xca, 0x0c,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            },
+            .merkle_root = [_]u8{
+                0x9d, 0x10, 0xaa, 0x52, 0xee, 0x94, 0x93, 0x86,
+                0xca, 0x93, 0x85, 0x69, 0x5f, 0x04, 0xed, 0xe2,
+                0x70, 0xdd, 0xa2, 0x08, 0x10, 0xde, 0xcd, 0x12,
+                0xbc, 0x9b, 0x04, 0x8a, 0xaa, 0xb3, 0x14, 0x71,
+            },
+            .timestamp = 1415239972,
+            .nbits = 404472624,
+            .nonce = 1678286846,
+        };
+
         try std.testing.expectEqual(1, deserialized_msg.headers.len);
-        try std.testing.expectEqual(2, deserialized_msg.headers[0].version);
-        try std.testing.expectEqual(1415239972, deserialized_msg.headers[0].timestamp);
-        try std.testing.expectEqual(1678286846, deserialized_msg.headers[0].nonce);
-        
+        try std.testing.expect(expected_block_header.eql(&deserialized_msg.headers[0]));
+
         const serialized_payload = try deserialized_msg.serialize(allocator);
         defer allocator.free(serialized_payload);
 
